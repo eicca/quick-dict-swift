bae7bcd55f6dcfc9d405c7387c40df9503586080
Mikhail Dyakov <wtltl2@gmail.com>
2014-06-07 13:33:58 +0200

    Initial Commit

---
+++ quick-dict-swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  quick-dict-swift
+//
+//  Created by Mike on 07.06.14.
+//  Copyright (c) 2014 tapok. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ quick-dict-swift/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  quick-dict-swift
+//
+//  Created by Mike on 07.06.14.
+//  Copyright (c) 2014 tapok. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
